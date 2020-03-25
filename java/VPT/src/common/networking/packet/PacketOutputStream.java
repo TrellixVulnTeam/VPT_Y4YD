38a15e6e31f0fc47d54a014da6c5c05e9f3ec04c
Ryan DeBarger <ryan.debarger@createthenext.com>
2020-03-25 09:33:08 -0700

    removed unneccesary import and updated gitignore

--- java/VPT/src/common/networking/packet/PacketOutputStream.java
+++ java/VPT/src/common/networking/packet/PacketOutputStream.java
@@ -3,7 +3,6 @@ package common.networking.packet;
 import common.Constants;
 import java.io.FilterOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.security.DigestOutputStream;
