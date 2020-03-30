4884d38c71f472d1a0fd95808de0d7151fc5263f
Ryan DeBarger <ryan.debarger@createthenext.com>
2020-03-29 17:23:52 -0700

    Added folder creation code

--- java/VPT/src/server/ServerMain.java
+++ java/VPT/src/server/ServerMain.java
@@ -3,12 +3,14 @@ package server;
 import common.Constants;
 import common.networking.AESServer;
 import common.networking.AESServerConnection;
+import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public final class ServerMain {
     
     public static void main(String[] args) {
+        createDirs();
         try {
             AESServer server = new AESServer(null);
             while(true) {
@@ -25,6 +27,16 @@ public final class ServerMain {
         } catch(IOException e) {
             e.printStackTrace(System.err);
         }
+    }
+    
+    private static void createDirs() {
+        createDir("");
+        createDir("Users");
+    }
+    
+    private static void createDir(String dir) {
+        new File(ServerConstants.SERVER_DIR + File.separator + dir.replaceAll("/", File.separator)).mkdirs();
+        new File(ServerConstants.BACKUP_DIR + File.separator + dir.replaceAll("/", File.separator)).mkdirs();
     }
 
     private ServerMain() {}
