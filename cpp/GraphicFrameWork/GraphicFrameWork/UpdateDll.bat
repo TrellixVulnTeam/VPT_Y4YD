b091f96dfca9ecf3e3413d771a91618c7e3bc67f
Ryan DeBarger <ryan.debarger@createthenext.com>
2020-06-04 18:46:43 -0700

    finalized relative paths

---
+++ cpp/GraphicFrameWork/GraphicFrameWork/UpdateDll.bat
@@ -0,0 +1,13 @@
+cd projects
+cd VPT
+cd JavaVPT
+DEL Client.dll
+cd ..
+cd ..
+cd ..
+cd ..
+cd x64
+cd Debug
+DEL Client.dll
+rename GraphicFrameWork.dll Client.dll
+replace Client.dll ..\..\GraphicFrameWork\projects\VPT\JavaVPT\src /A
