9030ce0adef77f1dcbb60d560314f007f0f52270
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2020-08-07 09:53:25 -0700

    updated ports

--- java/VPTPythonServer/src/core/Main.java
+++ java/VPTPythonServer/src/core/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
     public static final String SERVER_IP = "127.0.0.1";
     public static final int SERVER_PORT = 5555;
-    public static final int LISTEN_PORT = 636;
+    public static final int LISTEN_PORT = 7327;
     public static final String ERROR_RESPONCE = "";
     
     public static void main(String[] args) {
