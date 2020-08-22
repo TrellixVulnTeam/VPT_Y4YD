4804d94c384abd91173c3d4197a178b6af7ba657
KevinIsGay <benjaminvgehant@gmail.com>
2020-08-21 22:00:11 -0700

    moncao editor stuff

--- python/monaco-editor/static/packets.js
+++ python/monaco-editor/static/packets.js
@@ -1,6 +1,25 @@
-var get_settings_packets = {
-	request: "get-module",
-	sdata: "data",
-	module: "meditor",
-	module_request: "settings"
+function compose_settings_packet(meth, data){
+	if(meth === "send"){
+		return {
+			request: "get-module",
+			sdata: "data",
+			module: "meditor",
+			module_request: "send-settings"
+			settings: data
+		};
+	}
+	if(meth === "get"){
+
+		return {
+			request: "get-module",
+			sdata: "data",
+			module: "meditor",
+			module_request: "settings"
+		};
+
+	}
+}
+
+function compose_code_packet(code){
+
 }
