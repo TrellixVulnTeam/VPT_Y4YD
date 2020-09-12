475a1d2c26911ca7bb99354657f8bb1111f8ec5b
BestVG <61258106+BestVG@users.noreply.github.com>
2020-09-12 00:39:43 -0700

    Update test.cpp

--- python/ComplieExecServer/rcode/loaders/c++/test.cpp
+++ python/ComplieExecServer/rcode/loaders/c++/test.cpp
@@ -3,8 +3,8 @@
 #include <string>
 using namespace std;
 
-extern "C" void RUN(vector <string> conststring, vector<vector <string>> dstrings, vector<int> constint, vector<vector <int>>);
+extern "C" void RUN(vector <string> conststring, vector<vector <string>> dstrings, vector<int> constint, vector<vector <int>> dints);
 
-void RUN(vector <string> conststring, vector<vector <string>> dstrings, vector<int> constint, vector<vector <int>>){
+void RUN(vector <string> conststring, vector<vector <string>> dstrings, vector<int> constint, vector<vector <int>> dints){
 	cout << "test" << endl;
 }
